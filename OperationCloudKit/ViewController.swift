5b018fadbc0e853e88d9fb5eb2f6d20a7f8b5e97
Leo Tumwattana <leo.tumwattana@gmail.com>
2016-06-02 14:06:29 +0800

    Adds code to comment / uncomment after first run

--- OperationCloudKit/ViewController.swift
+++ OperationCloudKit/ViewController.swift
@@ -16,12 +16,24 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // ==============================================
+        // Initial run to make sure we have a zone create
+        // Note: You can comment this out after first run
+        // and uncomment the second part to skip running
+        // CreateZoneOperation()
+        // ==============================================
+        
         let createZone = CreateZoneOperation()
         let createRecord = CreateRecordOperation()
-        
         createRecord.addDependency(createZone)
-        
         queue.addOperations([createZone, createRecord])
+        
+        // ==============================================
+        // Uncomment this after first run
+        // ==============================================
+        
+        //let createRecord = CreateRecordOperation()
+        //queue.addOperations([createRecord])
     }
 
 }
